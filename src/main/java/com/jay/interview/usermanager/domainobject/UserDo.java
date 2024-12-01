930c8734327b0775489696edc6b194b85c47dd16
jay841224 <j1360fy6416nb@gmail.com>
2024-12-01 22:38:27 +0800

    add validate

--- src/main/java/com/jay/interview/usermanager/domainobject/UserDo.java
+++ src/main/java/com/jay/interview/usermanager/domainobject/UserDo.java
@@ -2,8 +2,10 @@ package com.jay.interview.usermanager.domainobject;
 
 import com.jay.interview.usermanager.validate.group.CreateGroup;
 import com.jay.interview.usermanager.validate.group.UpdateGroup;
+import jakarta.validation.constraints.Email;
 import jakarta.validation.constraints.NotNull;
 import jakarta.validation.constraints.Null;
+import jakarta.validation.constraints.Pattern;
 import lombok.Getter;
 import lombok.Setter;
 
@@ -19,8 +21,10 @@ public class UserDo {
     private String name;
 
     @NotNull(message = "email can not be null", groups = {CreateGroup.class, UpdateGroup.class})
+    @Email(message = "incorrect format of email", groups = {CreateGroup.class, UpdateGroup.class})
     private String email;
 
     @NotNull(message = "phone can not be null", groups = {CreateGroup.class, UpdateGroup.class})
+    @Pattern(regexp = "^[0-9]{10}$", message = "incorrect format of phone")
     private String phone;
 }
