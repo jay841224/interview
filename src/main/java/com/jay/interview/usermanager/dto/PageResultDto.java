0f5e83877c4f855bbc3d1a27b56bc2ae0daf9ced
jay841224 <j1360fy6416nb@gmail.com>
2024-12-01 22:46:28 +0800

    add dto schema

--- src/main/java/com/jay/interview/usermanager/dto/PageResultDto.java
+++ src/main/java/com/jay/interview/usermanager/dto/PageResultDto.java
@@ -1,5 +1,6 @@
 package com.jay.interview.usermanager.dto;
 
+import io.swagger.v3.oas.annotations.media.Schema;
 import lombok.Getter;
 import lombok.Setter;
 
@@ -9,13 +10,18 @@ import java.util.List;
 @Setter
 public class PageResultDto<T> {
 
+    @Schema(description = "query contents")
     private List<T> content;
 
+    @Schema(description = "number of content")
     private int totalElements;
 
+    @Schema(description = "total pages of contents")
     private int totalPages;
 
+    @Schema(description = "current page")
     private int number;
 
+    @Schema(description = "size of page")
     private int size;
 }
